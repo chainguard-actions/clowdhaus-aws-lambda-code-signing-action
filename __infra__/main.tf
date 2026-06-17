f9df3f7114571f55286e9f0cc154e26621cd2603
actionchain <actionchain@chainguard.dev>
2026-06-17 00:59:10 +0000

    Update clowdhaus/aws-lambda-code-signing-action@v1.5.2

---
+++ __infra__/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.17" # https://github.com/hashicorp/terraform-provider-aws/blob/main/CHANGELOG.md#3170-november-24-2020
+    }
+  }
+
+  # Be sure to change to your own backend when running locally
+  backend "s3" {
+    bucket         = "clowd-haus-iac-us-east-1"
+    key            = "aws-lambda-code-signing-action/us-east-1/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "clowd-haus-terraform-state"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+################################################################################
+# Common Locals
+################################################################################
+
+locals {
+  account_id  = data.aws_caller_identity.current.account_id
+  region      = data.aws_region.current.name
+  environment = "dev"
+  name        = "aws-lambda-code-signing-action"
+
+  tags = {
+    Repository  = "https://github.com/clowdhaus/${local.name}"
+    Environment = local.environment
+  }
+}
+
+################################################################################
+# Common Data
+################################################################################
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
